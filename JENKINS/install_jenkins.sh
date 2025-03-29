51259787e1517d1f78ab7f896c492db14511f392
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2025-03-29 08:40:31 -0500

    Update install_jenkins.sh

--- JENKINS/install_jenkins.sh
+++ JENKINS/install_jenkins.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 sudo apt update
-sudo apt install openjdk-11-jdk -y
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63667EE74BBA1F0A08A698725BA31D57EF5975CA
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install openjdk-17-jre-headless -y
 sudo apt install ca-certificates
 sudo apt update
-sudo apt install git -y
 sudo apt install maven -y
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
 sudo apt-get update -y
@@ -27,3 +26,4 @@ echo 'clearing screen...' && sleep 5
 clear
 echo 'jenkins is installed'
 echo 'this is the default password :' $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+sudo reboot
